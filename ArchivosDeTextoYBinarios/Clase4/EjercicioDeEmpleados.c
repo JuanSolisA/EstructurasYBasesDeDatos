6d2e4d6af698de077e116a0ba49aa54bf1a3f407
JuanSolisA <juan.solis.trabajo@gmail.com>
2024-05-04 18:43:33 -0300

    Agregando funcionalidad para mostrar el contenido del archivo de texto Empleado.cvs

--- ArchivosDeTextoYBinarios/Clase4/EjercicioDeEmpleados.c
+++ ArchivosDeTextoYBinarios/Clase4/EjercicioDeEmpleados.c
@@ -7,14 +7,71 @@ Opción 2: Generar un archivo binario que contenga el nombre de los empleados y 
  Opción 3: Generar un informe con los que tienen oficio="Empleado"  (empleados.txt)
  y otro con los demas empleados.    (otro.txt)
  */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
+void mostrarArchivoTexto()
+{
+    FILE *archivo;
+    char linea[100];
 
+    archivo = fopen("empleados.csv", "r");
+    if (archivo == NULL)
+    {
+        printf("Error al abrir el archivo.\n");
+        return;
+    }
 
+    printf("Contenido del archivo Empleado.cvs:\n");
+    printf("Num de Empleado\tEmpleado\tPuesto\tSalario\tHobby\n\n");
+
+    while (fgets(linea, sizeof(linea), archivo) != NULL)
+    {
+        char *token = strtok(linea, ";");
+        while (token != NULL)
+        {
+            printf("%s\t", token);
+            token = strtok(NULL, ";");
+        }
+        printf("\n");
+    }
+
+    fclose(archivo);
+    system("pause");
+}
+
+void generarArchivoBinarioVentas() {}
+
+void generarInformesSeparados() {}
 
 int main()
 {
-    
+    int opcion;
+
+    printf("Ejercicio de procesamiento de archivos Empleado.csv\n");
+    printf("1. Mostrar el archivo de texto\n");
+    printf("2. Generar un archivo binario con empleados del sector de ventas\n");
+    printf("3. Generar informes separados en archivos de texto\n");
+    printf("Ingrese el numero de la opcion que desea ejecutar: ");
+
+    scanf("%d", &opcion);
+
+    switch (opcion)
+    {
+    case 1:
+        mostrarArchivoTexto();
+        break;
+    case 2:
+        generarArchivoBinarioVentas();
+        break;
+    case 3:
+        generarInformesSeparados();
+        break;
+    default:
+        printf("Opcion no valida\n");
+        break;
+    }
+    return 0;
 }
