676eda402a3029f78250608e04b5d4c25379efcd
JuanSolisA <juan.solis.trabajo@gmail.com>
2024-05-06 11:33:46 -0300

    feat: Agregar manejo de archivos binarios para guardar y leer datos de personas

--- Archivos Binarios-20240506/BinarioStruct1.c
+++ Archivos Binarios-20240506/BinarioStruct1.c
@@ -2,42 +2,41 @@
 #include <stdio.h>
 #include <string.h>
 
-typedef struct {
-				  char nombre[25];
-				  int edad;
-				 }Persona;
-
-int main(){
-
-int i=0;
-FILE *pArchivo;
-Persona per;
-pArchivo=fopen("datos.dat","wb");
-if(pArchivo!=NULL)
+typedef struct
 {
-				
-				fflush(stdin);
-				printf("Introduzca el nombre de la persona: ");
+	char nombre[25];
+	int edad;
+} Persona;
 
-				scanf("%s",per.nombre);
-				while( strcmp(per.nombre, "*")!=0)
-                {
-											printf("Introduzca la edad ");
-											scanf("%d",&(per.edad));
-											fwrite(&per,sizeof(Persona),1,pArchivo);
-											printf("Introduzca el nombre de la persona: ");
-			                             	scanf("%s",per.nombre);
-											
-		     	}
-fclose(pArchivo); 	     	
+int main()
+{
 
-    
-   
-  }
+	int i = 0;
+	FILE *pArchivo;
+	Persona per;
+	pArchivo = fopen("datos.dat", "wb");
+	if (pArchivo != NULL)
+	{
 
+		fflush(stdin);
+		printf("Introduzca el nombre de la persona: ");
 
+		scanf("%s", per.nombre);
+		while (strcmp(per.nombre, "*") != 0)
+		{
+			printf("Introduzca la edad ");
+			scanf("%d", &(per.edad));
+			fwrite(&per, sizeof(Persona), 1, pArchivo);
+			printf("Introduzca el nombre de la persona: ");
+			scanf("%s", per.nombre);
+		}
+		fclose(pArchivo);
+	}
 
-else{printf("Error en la apertura del archivo");}
-system("pause");
-return 0;
+	else
+	{
+		printf("Error en la apertura del archivo");
+	}
+	system("pause");
+	return 0;
 }
--- Explicacion Archivos de texto-20240506/3.Lee texto por linea.c
+++ Explicacion Archivos de texto-20240506/3.Lee texto por linea.c
@@ -1,21 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(){
-FILE *archivo;
-char texto[2000];
-archivo=fopen("../texto2.txt","r");
-if(archivo!=NULL) {
-	fgets(texto,2000,archivo); //carga una l�nea
-				while(!feof(archivo))
-						{
-						puts(texto);//prueben con printf
-						fgets(texto,2000,archivo);// probar con fscanf
-						}
-	fclose(archivo);
-}
-else{printf("Error en la apertura");}
+int main()
+{
+	FILE *archivo;
+	char texto[2000];
+	archivo = fopen("./texto2.txt", "r");
+	if (archivo != NULL)
+	{
+		fgets(texto, 2000, archivo); // carga una l�nea
+		while (!feof(archivo))
+		{
+			puts(texto);				 // prueben con printf
+			fgets(texto, 2000, archivo); // probar con fscanf
+		}
+		fclose(archivo);
+	}
+	else
+	{
+		printf("Error en la apertura");
+	}
 
-system("pause");
-return 0;
+	system("pause");
+	return 0;
 }
